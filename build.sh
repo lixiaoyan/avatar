50198bd6678564d102623d929f11460f18b856f2
IntPtr <lxy.lixiaoyan@gmail.com>
2017-01-30 06:14:42 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+inkscape -w 1000 -h 1000 -e avatar-1.png avatar-1.svg
+inkscape -w 1000 -h 1000 -e avatar-2.png avatar-2.svg
+inkscape -w 1000 -h 1000 -e avatar-2.1.png avatar-2.1.svg
+inkscape -w 1000 -h 1000 -e avatar-3.png avatar-3.svg
